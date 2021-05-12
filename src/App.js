f650e1b0f52254a1692b176c2de5d7d78f36fa74
Adrian Aburto <A01731751@itesm.mx>
2021-05-12 08:17:23 -0500

    display de mapa

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 // Link, Redirect
 import LandingPage from './components/landing-page/landing-page.component'
 import Dashboard from './components/dashboard/dashboard.component'
+import Covid19 from  './components/dashboard/Covid19'
 
 function App () {
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Dashboard} />
-          <Route exact path='/acerca_de' component={LandingPage} />
-        </Switch>
-      </Router>
+     
+        <Covid19 />
+     
     </div>
+  
   )
 }
 
