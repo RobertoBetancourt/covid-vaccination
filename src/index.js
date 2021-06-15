8c2c789437f003b567749ddcdbe31b0215889c0a
Roberto Betancourt <rob_betancourt@hotmail.com>
2021-06-15 09:47:07 -0500

    Added GraphQL and handled errors

--- src/index.js
+++ src/index.js
@@ -2,8 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import * as serviceWorker from './serviceWorker'
+
+// 1
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql'
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 )
+serviceWorker.unregister()
